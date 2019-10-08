5fa82f380ca7155bcdc137b80f1f845d92e4ddc8
Lukas Paleckis <lukaspaleckis@gmail.com>
2019-10-08 12:34:50 +0300

    jau gali patikrinti kokia kaina puslapyje

--- main.py
+++ main.py
@@ -1,7 +1,55 @@
-price = 20.0
-desired_price = 15.0
-email = 'pastas@gmail.com'
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+import time
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+    server.login('lukaspaleckis@gmail.com', 'zheocwkwiofpnvjh')
+
+    body = 'Perziurek cia: https://www.topocentras.lt/kompiuteriai-ir-plansetes/nesiojamieji-kompiuteriai/nesiojamas-kompiuteris-acer-predator-helios-300-ph315-51-i7-8750h-16-1tb-128gb-ssd-gtx1050ti-4gb-win.html'
+
+    msg = f'Subject: {alert}\n\n{body}'
+    server.sendmail(
+        'lukaspaleckis@gmail.com',
+        'kulamburas@yahoo.com',
+        msg
+    )
+    print('Email sent')
+    server.quit()
+
+
+desired_price = 800.00
+user_mail = 'pastas@gmail.com'
 alert = 'pranesimas del kainos'
-website_url = 'https://'
-website_output = 'html'
-time = 12
+
+
+def check_product_price():
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+    website_url = requests.get(
+        'https://www.topocentras.lt/kompiuteriai-ir-plansetes/nesiojamieji-kompiuteriai/nesiojamas-kompiuteris-acer-predator-helios-300-ph315-51-i7-8750h-16-1tb-128gb-ssd-gtx1050ti-4gb-win.html', headers=headers).text
+    soup = BeautifulSoup(website_url, 'html.parser')
+    price = soup.find('span', id='price-including-tax-165500').get_text()
+    price = price.replace('€', '').replace(',', '.')
+    price = price.strip()
+    price = float(price)
+
+    if price <= desired_price:
+        send_mail()
+    else:
+        print('nepasikeite')
+        print(price)
+
+
+while(True):
+    check_product_price()
+    time.sleep(86400)
+
+#time = 12
